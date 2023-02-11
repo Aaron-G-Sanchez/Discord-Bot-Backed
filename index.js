0a0c959e24a1e2726c567f6dc6d6487554e81e16
Aaron Sanchez <saarong12@gmail.com>
2023-02-11 12:37:33 -0600

    Basic index file set up

---
+++ index.js
@@ -0,0 +1,30 @@
+import { REST, Routes } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const token = process.env.TOKEN
+
+const commands = [
+  {
+    name: 'Ping',
+    description: 'Replies with pong!'
+  }
+]
+
+const rest = new REST({ version: '10' }).setToken(token)
+
+const test = async () => {
+  try {
+    console.log('Started refreshing application (/) commands.')
+
+    await rest.put(Routes.applicationCommands(clientID), {
+      body: commands
+    })
+
+    console.log('Successfully reloaded application (/) commands.')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+test()
