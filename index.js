b9750ccfc17901e26a3dcca44a0f0559fb38e5df
Aaron Sanchez <saarong12@gmail.com>
2023-02-11 16:23:39 -0600

    Index file set up

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-import { REST, Routes } from 'discord.js'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const token = process.env.TOKEN
-const clientID = process.env.CLIENT_ID
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with pong!'
-  },
-  {
-    name: 'hello',
-    description: 'Replies with world!'
-  }
-]
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-const rest = new REST({ version: '10' }).setToken(token)
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
 
-const test = async () => {
-  try {
-    console.log('Started refreshing application (/) commands.')
-
-    await rest.put(Routes.applicationCommands(clientID), {
-      body: commands
-    })
-
-    console.log('Successfully reloaded application (/) commands.')
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-test()
+client.login(token)
