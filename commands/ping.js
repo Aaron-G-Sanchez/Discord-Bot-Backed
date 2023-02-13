0ab38a7ab589f40a661746e30536decc9b207fca
Aaron Sanchez <saarong12@gmail.com>
2023-02-13 12:02:57 -0600

    Changed reply to a variable

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js')
 
+const reply = 'pong'
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!')
+    await interaction.reply(reply)
   }
 }
