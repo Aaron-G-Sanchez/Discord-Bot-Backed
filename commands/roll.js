8253374ab062a991f3d30aafd00096f32b06af3d
Aaron Sanchez <saarong12@gmail.com>
2023-02-14 14:34:20 -0600

    Roll command generates a random number based off a 6 sided dice

--- commands/roll.js
+++ commands/roll.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js')
+// const wait = require('node:timers/promises').setTimeout
+
+const dice = [1, 2, 3, 4, 5, 6]
+let rollResult = 0
+
+const rollDice = () => {
+  const roll = Math.floor(Math.random() * dice.length) + 1
+  rollResult = roll
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('roll')
     .setDescription('Rolls a dice!'),
   async execute(interaction) {
-    await interaction.reply('rolling')
+    rollDice()
+    await interaction.reply(rollResult.toString())
+    // await wait(1000)
+    // await interaction.editReply(rollResult)
   }
 }
