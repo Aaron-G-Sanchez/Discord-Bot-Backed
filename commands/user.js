55235154d84084d6701fb34f3944c1e95a009eda
Aaron Sanchez <saarong12@gmail.com>
2023-03-08 16:29:30 -0600

    Slash command responds with A user from the database. Need to return all the users

--- commands/user.js
+++ commands/user.js
@@ -3,14 +3,17 @@ const axios = require('axios')
 
 const getUsers = async () => {
   const users = await axios.get('http://localhost:3001/users')
-  console.log(users.data.users)
+  // console.log(users.data.users)
+  return users.data.users
 }
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName('users')
-//     .setDescription('Returns all TK offenders'),
-//   async execute(interaction) {}
-// }
-
-getUsers()
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('users')
+    .setDescription('Returns all TK offenders'),
+  async execute(interaction) {
+    let response = await getUsers()
+    console.log(response)
+    await interaction.reply(response[0].name)
+  }
+}
